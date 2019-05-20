a92be9db12887c2a7a2d0424e716e1fbb37b5dbd
MikelDementyev <mike.new.steam@gmail.com>
2019-05-20 14:56:25 +0300

    .

---
+++ android_public_speech_trainer/app/src/main/java/ru/spb/speech/RecomendationActivity.kt
@@ -0,0 +1,12 @@
+package ru.spb.speech
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RecomendationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recomendation)
+    }
+}
